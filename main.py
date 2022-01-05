df346d813c7b934b4349de3979f8422d20a50d09
benjamin-elias <benni.probst@gmx.de>
2022-01-05 17:35:33 +0100

    test map functions to add required layers

--- main.py
+++ main.py
@@ -3,14 +3,18 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import leafmap
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print('Starting first leafmap test...')
+    # center to New York at 41 degrees north and 74 degrees west
+    m = leafmap.Map(google_map="HYBRID", center=[40.702557, -74.012318], zoom=12, height="450px",width="800px")  # center=[lat, lon]
+    # add a point layer for the taxi data
+
+    # add a chart layer and legends for the taxi statistics
+
+    # show
+    m
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
