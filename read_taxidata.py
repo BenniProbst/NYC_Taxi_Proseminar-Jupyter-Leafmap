df1bc4f9368cb23763d5f01637e797c8ad9fb832
benjamin-elias <benni.probst@gmx.de>
2022-01-09 17:31:25 +0100

    read taxi data

--- read_taxidata.py
+++ read_taxidata.py
@@ -1,16 +1,22 @@
 # open file in read mode
 from csv import reader
 from typing import Tuple
+from typing import NewType
+from typing import Tuple
+from datetime import datetime
 
-#TODO create loading class with type hints
 
-# open file in read mode
-with open('/home/benjamin-elias/Proseminar/Jupyterlab/taxi_data/yellow_tripdata_2021-07.csv', 'r') as read_obj:
-    # pass the file object to reader() to get the reader object
-    csv_reader = reader(read_obj)
-    # Get all rows of csv from csv_reader object as list of tuples
-    # list_of_tuples = list(map(tuple, csv_reader))
-    list_of_tuples = list(map(tuple, csv_reader))
+# TODO create loading class with type hints
 
-    # display all rows of csv
-    # print(list_of_tuples)
+
+class TaxiData:
+    def __init__(self):
+        self.data = NewType('taxi_data', Tuple[int, datetime, datetime, int, float, int, str, int, int, int, float,
+                                               float, float, float, float, float, float, float])
+        # open file in read mode
+        with open('/home/benjamin-elias/Proseminar/Jupyterlab/taxi_data/yellow_tripdata_2021-07.csv', 'r') as read_obj:
+            # pass the file object to reader() to get the reader object
+            csv_reader = reader(read_obj)
+            # Get all rows of csv from csv_reader object as list of tuples
+            # list_of_tuples = list(map(tuple, csv_reader))
+            list_of_tuples_load = list(map(tuple, csv_reader))
