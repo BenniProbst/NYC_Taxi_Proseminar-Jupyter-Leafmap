d20e45be8004aa12f193de5d6527bd0eb469a8df
benjamin-elias <benni.probst@gmx.de>
2022-01-10 15:03:01 +0100

    max

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ import read_taxidata
 if __name__ == '__main__':
     print('Starting first leafmap test...')
     td = read_taxidata.TaxiData('/home/benjamin-elias/Proseminar/Jupyterlab/taxi_data/')
-    td.load_add_available(td.get_date_files(2021, 7))
+    td.load_add_available(td.get_date_files(2021, 3))
+    print(td.get_minimum_available_pickup_time())
+    print(td.get_maximum_available_pickup_time())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
