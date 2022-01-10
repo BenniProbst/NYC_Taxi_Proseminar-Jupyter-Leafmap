4176e80b9e214a83861c40ba705518a95c4f0e53
benjamin-elias <benni.probst@gmx.de>
2022-01-10 22:15:41 +0100

    random colors map

--- main.py
+++ main.py
@@ -8,12 +8,23 @@ from leafmap import leafmap
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print('Starting first leafmap test...')
-    tz = read_taxizone.TaxiZone('/home/benjamin-elias/Proseminar/Jupyterlab/taxi_data/taxi+_zone_lookup.csv')
-    print(tz.get_location(29))
-    td = read_taxidata.TaxiData('/home/benjamin-elias/Proseminar/Jupyterlab/taxi_data/')
-    td.load_add_available(td.get_date_files(2021, 3))
-    print(td.get_minimum_available_pickup_time())
-    print(td.get_maximum_available_pickup_time())
+    # print('Starting first leafmap test...')
+    # tz = read_taxizone.TaxiZone('/home/benjamin-elias/Proseminar/Jupyterlab/taxi_data/taxi+_zone_lookup.csv')
+    # print(tz.get_location(29))
+    # td = read_taxidata.TaxiData('/home/benjamin-elias/Proseminar/Jupyterlab/taxi_data/')
+    # td.load_add_available(td.get_date_files(2021, 3))
+    # print(td.get_minimum_available_pickup_time())
+    # print(td.get_maximum_available_pickup_time())
+
+    m = leafmap.Map(google_map="HYBRID", center=[40.702557, -74.012318], zoom=12, height="600px", width="1200px",
+                    max_zoom="20")  # center=[lat, lon]
+    # show
+    print('Showing current layer names before adding neighbourhoods:')
+    print(m.get_layer_names())
+    print('Add geojson layer neighbourhoods:')
+    m.add_geojson('/home/benjamin-elias/Proseminar/Jupyterlab/nyc_neighborhoods.geo.json', layer_name='neighbourhoods',
+                  style={}, hover_style={}, style_callback=None, fill_colors=['red'], info_mode='on_hover')
+    print(m.get_layer_names())
+    m
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
