e97b37056f9907d49f5ddcb3bf169084ea56fc50
benjamin-elias <benni.probst@gmx.de>
2022-01-11 21:00:32 +0100

    neighbourhood data name variants

--- read_neighbourhoods.py
+++ read_neighbourhoods.py
@@ -9,10 +9,12 @@ class NeighbourhoodData:
     def __init__(self, path):
         tz = read_taxizone.TaxiZone('/home/benjamin-elias/Proseminar/Jupyterlab/taxi_data/taxi+_zone_lookup.csv')
         self.neighbourhoodTuples: List[Tuple[int, str, str, str]] = []
+        # multiple neighborhood polynoms for every taxi zone
         self.neighbourhoodPolynoms: List[List[Tuple[float, float]]] = []
         tmp_list = json.loads(open(path, 'r').read())
         for features in tmp_list['features']:
-            data_tuple: Tuple[int, str, str, str] = tz.get_alike_from_neighborhood_name(features['properties']['NTAName'])
+            data_tuple: Tuple[int, str, str, str] = \
+                tz.get_alike_from_neighborhood_name(features['properties']['NTAName'])
             polygon_list: List[Tuple[float, float]] = []
             if features['geometry']['type'] == 'MultiPolygon':
                 for point in features['geometry']['coordinates'][0][0]:
@@ -21,11 +23,9 @@ class NeighbourhoodData:
                 if features['geometry']['type'] == 'Polygon':
                     for point in features['geometry']['coordinates'][0]:
                         polygon_list.append((float(point[0]), float(point[1])))
-            new_data_tuple: Tuple[int, str, str, str]
-            if features['properties']['NTAName'] != data_tuple[2]:
-                new_data_tuple = (data_tuple[0], data_tuple[1], features['properties']['NTAName'], data_tuple[3])
-            else:
-                new_data_tuple = data_tuple
 
-            self.neighbourhoodTuples.append(new_data_tuple)
-            self.neighbourhoodPolynoms.append(polygon_list)
+            if data_tuple in self.neighbourhoodTuples:
+                self.neighbourhoodPolynoms[self.neighbourhoodTuples.index(data_tuple)].append(polygon_list)
+            else:
+                self.neighbourhoodTuples.append(data_tuple)
+                self.neighbourhoodPolynoms.append(polygon_list)
