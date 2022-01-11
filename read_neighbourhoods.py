fde419c7095adaeb857917a405dfd75254f5f91a
benjamin-elias <benni.probst@gmx.de>
2022-01-11 11:20:07 +0100

    neighbourhood data, read zone polygons

--- read_neighbourhoods.py
+++ read_neighbourhoods.py
@@ -2,16 +2,25 @@ import json
 from typing import Dict
 from typing import List
 from typing import Tuple
+from typing import Union
 import read_taxizone
+from decimal import Decimal
 
 
 class NeighbourhoodData:
 
     def __init__(self, path):
         tz = read_taxizone.TaxiZone('/home/benjamin-elias/Proseminar/Jupyterlab/taxi_data/taxi+_zone_lookup.csv')
-        self.neighbourhoodPolynoms: Dict[Tuple[int, str, str, str]: List[Tuple[float, float]]] = []
+        self.neighbourhoodPolynoms: Dict[Tuple[int, str, str, str]: List[Tuple[Decimal, Decimal]]] = []
         tmp_list = json.loads(open(path, 'r').read())
         for features in tmp_list['features']:
-            polygon_list: List[Tuple[float, float]] = []
+            polygon_list: List[Tuple[Decimal, Decimal]] = []
             for point in features['geometry']['coordinates'][0][0]:
-                pass
+                polygon_list.append((Decimal(point[0]), Decimal(point[1])))
+            neighbourhood_data: Union[Tuple[int, str, str, str], None] = \
+                tz.get_from_neighbourhood_name(features['properties']['neighborhood'])
+
+            if neighbourhood_data is None:
+                continue
+
+            self.neighbourhoodPolynoms[neighbourhood_data] = polygon_list
