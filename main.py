6c7274688269d123ef2046439232db10adc20763
benjamin-elias <benni.probst@gmx.de>
2022-01-13 16:58:59 +0100

    read_taxi_geojson

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from read_neighbourhoods import NeighbourhoodTaxiData
 import read_taxidata
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    
+    ntd = NeighbourhoodTaxiData('/home/benjamin-elias/PycharmProjects/Proseminar '
+                                'Jupyter-Leafmap/nyc-neighborhoods.geo.json')
+    print(ntd.central_points())
+    ntd.to_geojson('/home/benjamin-elias/PycharmProjects/Proseminar Jupyter-Leafmap/taxi_zones.geo.json')
     pass
 
 
