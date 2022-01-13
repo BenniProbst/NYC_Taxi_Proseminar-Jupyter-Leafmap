76cd3b702e3b4ef8a0bc82423a0e0f34452b4ee1
benjamin-elias <benni.probst@gmx.de>
2022-01-13 18:52:47 +0100

    TaxiTime

--- get_drives_by_time.py
+++ get_drives_by_time.py
@@ -1,5 +1,6 @@
 from read_taxidata import TaxiData
 from read_neighbourhoods import NeighbourhoodTaxiData
+import datetime
 
 
 class TaxiTime:
@@ -9,4 +10,5 @@ class TaxiTime:
         self.zone_neighborhoods.to_geojson('/home/benjamin-elias/PycharmProjects/Proseminar '
                                            'Jupyter-Leafmap/taxi_zones.geo.json')
         self.big_taxi_data = TaxiData('/home/benjamin-elias/Proseminar/Jupyterlab/taxi_data/')
-        pass
+        self.min_time: datetime = self.big_taxi_data.get_minimum_available_pickup_time()
+        self.max_time: datetime = self.big_taxi_data.get_maximum_available_pickup_time()
