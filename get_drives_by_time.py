06823f33c8a49b8559c326f20b2e8143b65183c4
benjamin-elias <benni.probst@gmx.de>
2022-01-20 03:01:09 +0100

    TaxiTime, all into modules get_drives_by_time.py and read_taxidata.py, ranges debug, inheritance

--- get_drives_by_time.py
+++ get_drives_by_time.py
@@ -3,9 +3,8 @@ from read_neighbourhoods import NeighbourhoodTaxiData
 import datetime
 
 
-class TaxiTime:
-    def __init__(self):
+class TaxiTime(TaxiData):
+    def __init__(self, base: str):
+        super().__init__(base)
         self.zone_neighborhoods = NeighbourhoodTaxiData('/home/benjamin-elias/PycharmProjects/Proseminar '
                                                         'Jupyter-Leafmap/nyc-neighborhoods.geojson')
-        self.big_taxi_data = TaxiData('/home/benjamin-elias/Proseminar/Jupyterlab/taxi_data/')
-        pass
--- main.py
+++ main.py
@@ -5,7 +5,7 @@
 from get_drives_by_time import TaxiTime
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    tt = TaxiTime()
+    tt = TaxiTime('/home/benjamin-elias/Proseminar/Jupyterlab/taxi_data/')
     pass
 
 
