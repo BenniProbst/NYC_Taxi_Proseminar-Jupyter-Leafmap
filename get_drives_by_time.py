9377a5dc7c2b4c5eb9ec414e795ab093686dd8d2
benjamin-elias <benni.probst@gmx.de>
2022-01-21 01:05:45 +0100

    TaxiTime interconnections

--- get_drives_by_time.py
+++ get_drives_by_time.py
@@ -15,7 +15,7 @@ class TaxiTime(TaxiData):
         for d in self.data:
             start_feature: Feature = self.zone_neighborhoods.get_feature(d[7])
             end_feature: Feature = self.zone_neighborhoods.get_feature(d[8])
-            prop = {'start': start_feature['properties'], 'end': end_feature['properties']}
+            prop = {'start': start_feature['properties'], 'end': end_feature['properties'], 'data': d}
             start_point: tuple = (start_feature['properties']['center'][0], start_feature['properties']['center'][1])
             end_point: tuple = (end_feature['properties']['center'][0], end_feature['properties']['center'][1])
             features.append(Feature(id=line_count, geometry=LineString([start_point, end_point]), properties=prop))
