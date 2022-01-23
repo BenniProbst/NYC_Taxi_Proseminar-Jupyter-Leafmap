96ce644a74ceb852d6ab7b36acb47d5d0fa9ca8d
benjamin-elias <benni.probst@gmx.de>
2022-01-23 19:02:14 +0100

    Heatmapper

--- con_geojson_to_heatmap.py
+++ con_geojson_to_heatmap.py
@@ -2,9 +2,9 @@ import geojson
 
 # give a FeatureCollection with LineStrings and their properties to build a Heatmap with
 class Heatmapper:
-    def __init__(self, path):
-        tmp_list = None
-        with open(path, 'r') as in_file:
-            tmp_list = geojson.load(in_file)
+    def __init__(self, path_in_geojson_con):
+        self.loaded_connections = None
+        with open(path_in_geojson_con, 'r') as in_file:
+            self.loaded_connections = geojson.load(in_file)
             in_file.close()
 
